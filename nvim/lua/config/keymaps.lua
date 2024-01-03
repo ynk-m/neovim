f70d06c1968a5056efa41396ae5cf29dbc71304b
ynk-m <107495122+ynk-m@users.noreply.github.com>
2024-01-03 01:13:49 +0100

    update
    Neue colorschemes hinzugefügt und Kurzbefehle hinzugefügt

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,11 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({ "i", "v" }, "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
--- vim.keymap.set("n", "<leader>o", "o<ESC>", { noremap = true, silent = true })
--- vim.keymap.set("n", "<leader>O", "O<ESC>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>o", "o<ESC>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>O", "O<ESC>", { noremap = true, silent = true })
 
 -- Coderunner keymaps
 vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false })
 vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
+
+--Add new Empty Line Above/Below
+vim.keymap.set("n", "<leader>O", "O<ESC>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>o", "o<ESC>", { noremap = true, silent = false })
+
+-- jump to start/end of line
+vim.keymap.set({ "v", "n" }, "H", "^", { noremap = true, silent = false })
+vim.keymap.set({ "v", "n" }, "L", "$", { noremap = true, silent = false })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,75 @@
 return {
-  { "craftzdog/solarized-osaka.nvim" },
-
-  --configure Lazyvim to load solarized-osaka
+  -- select colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "tokyonight",
+    },
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = true,
+      terminal_colors = true,
+      background = "dark",
+    },
+  },
+
+  {
+    "craftzdog/solarized-osaka.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      term_colors = true,
+      transparent_background = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
     },
   },
 }
