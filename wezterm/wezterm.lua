668a4e4967945c88fc89fd18beba678a5026b79d
ynk-m <matousekyannick@gmail.com>
2024-01-19 18:48:04 +0100

    Create wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catppuccin Frappe"
+-- Font
+config.font =
+	wezterm.font("BigBlueTerm437 Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
+-- transparency
+config.window_background_opacity = 0.95
+
+-- and finally, return the configuration to wezterm
+return config
